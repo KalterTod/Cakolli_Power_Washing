37d4dd922134e7dc6983b9209928693ee9bd96ac
Stephen Clark <stephenclark@MOBImage.local>
2014-04-28 09:30:28 -0400

    Updated javascript for e-mail form

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-function hgsubmit() {
-  $.ajax({
-  type: “POST”,
-  url: “https://mandrillapp.com/api/1.0/messages/send.json”,
-  data: {
-    ‘key’: ‘0_ucNb6mRKcKlM_HctQ8KA’,
-    ‘message’: {
-      ‘from_email’: 'thereaper666@gmail.com',
-      ‘to’: [
+function submit_email(){
+  var xhr = new XMLHttpRequest();
+  var content, name, email;
+  name=document.getElementById("person_name");
+  content=document.getElementById("comment");
+  email=document.getElementById("email");
+  xhr.open("POST", "https://mandrillapp.com/api/1.0/messages/send.json", true);
+  console.log("Here");
+  xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");
+  // Add your code below!
+  xhr.send(
+    {
+    "key": "QNl-KWAaxMNqr3APX7bPOw",
+    "message": {
+      "text":content,
+      "subject": "New Message",
+      "from_email": email,
+      "from_name": name,
+      "to": [
           {
-            ‘email’: ‘thereaper666@gmail.com’,
-            ‘type’: ‘to’
-          }
-        ],
-      ‘autotext’: ‘true’,
-      ‘subject’: ‘New message’,
-      ‘html’: comment
-    }
-  }
- }).done(function(response) {
-   console.log(response); // if you're into that sorta thing
- });
+              "email": "thereaper666@gmail.com",
+              "name": "Cakolli Power Washing",
+              "type": "to"
+            }
+            ]
+      }
+  });
+  console.log(xhr.status);
+  console.log(xhr.statusText);
 }
+
+submit_email();
